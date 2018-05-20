b30baa2e87e37dc70e883ddf0a10873b2fd2ab4b
Josh Cesana <joshuacesana@gmail.com>
2018-05-20 22:23:07 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
      }
   
   programmer_hash[:grace_hopper]
-  # programmer_hash[:alan_kay][:known_for]
+  programmer_hash[:alan_kay][:known_for]
   # programmer_hash[:josh_cesana] = "new student"
   # programmer_hash[:josh_cesana]
   # programmer_hash[:alan_kay][:known_for] = "RUBY"
